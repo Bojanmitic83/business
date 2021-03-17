31cb962f7ae9fff3a7c5a91b776651ea71c12f3d
Bojanmitic83 <bojanmitic83@gmail.com>
2021-03-17 19:28:32 +0100

    konačna

--- index.php
+++ index.php
@@ -1,14 +1,151 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Business</title>
-     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet"> <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet"> 
-     <link rel="stylesheet" href="scss/style.css">
-</head>
-<body>
+<?php include "partials/header.php" ?>
 
-</body>
-</html>
+    <!-- header -->
+    <header class="mainHeader">
+        <h1>Trusted and <br> professionals advisers</h1>
+        <h3>For your business</h3>
+        <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+        <button>About us</button>
+    </header>
+
+    <!-- headings -->
+    <section class="container headings">
+        <article>
+            <h2>01</h2>
+        </article>
+        <article>
+            <h3>About us</h3>
+            <h2><span>WE CAN</span> HELP YOUR BUSINESS GROW</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+
+    <!-- about -->
+    <section class="about">
+        <article>
+            <img src="img/about.jpg" alt="">
+        </article>
+        <article>
+            <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eos incidunt aperiam, dolorem. Iste voluptatibus repellendus omnis, perferendis beatae quasi dicta possimus similique ad, facilis nemo! Velit molestias blanditiis deleniti voluptatum cupiditate ullam alias magnam, soluta doloremque distinctio optio repellendus voluptas voluptatibus nesciunt ab modi, facilis possimus incidunt iste, ad?</p>
+        </article>
+    </section>
+
+    <!-- headings -->
+    <section class="container headings">
+        <article>
+            <h2>02</h2>
+        </article>
+        <article>
+            <h3>Services</h3>
+            <h2><span>HOW TO PROVIDE</span> TOOLS THAT HELP ANYONE</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+
+    <!-- services -->
+    <section class="services container">
+        <article>
+            <i class="far fa-chart-bar"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-bullseye"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-map-signs"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-filter"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-hand-holding-usd"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+        <article>
+            <i class="fas fa-bullhorn"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ullam illo, perferendis qui in rem consequatur omnis cumque saepe, repellendus eaque. Labore ducimus modi dolorum voluptates sequi iste praesentium, omnis.</p>
+        </article>
+    </section>
+
+     <!-- headings -->
+     <section class="business">
+        <header class="container headings black">
+            <article>
+                <h2>03</h2>
+            </article>
+            <article>
+                <h3>Services</h3>
+                <h2><span>HOW TO PROVIDE</span> TOOLS THAT HELP ANYONE</h2>
+                <h4>Get your Business in 4 easy steps</h4>
+                <button>About us</button>
+            </article>
+        </header>
+        <div class="counter container">
+            <div class="col">
+                <h2>18</h2>
+                <h4>Nominations</h4>
+                <hr>
+            </div>
+            <div class="col">
+                <h2>5</h2>
+                <h4>Awwards</h4>
+                <hr>
+            </div>
+            <div class="col">
+                <h2>45</h2>
+                <h4>Agencies</h4>
+                <hr>
+            </div>
+        </div>
+     </section>
+
+    <!-- headings -->
+    <section class="container headings">
+        <article>
+            <h2>04</h2>
+        </article>
+        <article>
+            <h3>Our team</h3>
+            <h2><span>We Have a Professional</span> Team of Business Analysts.</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+
+     <!-- team -->
+     <section class="team container">
+         <article>
+             <img src="img/team1.jpg" alt="">
+             <h4>James Douglas</h4>
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur optio molestiae quaerat ratione cupiditate sint voluptatem debitis rerum, ut at.</p>
+         </article>
+         <article>
+             <img src="img/team2.jpg" alt="">
+             <h4>Peter Green</h4>
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur optio molestiae quaerat ratione cupiditate sint voluptatem debitis rerum, ut at.</p>
+         </article>
+         <article>
+             <img src="img/team3.jpg" alt="">
+             <h4>Etta James</h4>
+             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur optio molestiae quaerat ratione cupiditate sint voluptatem debitis rerum, ut at.</p>
+         </article>
+     </section>
+
+<?php include "partials/footer.php" ?>
+
+    
